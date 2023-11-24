722c0f101aa65d8c719b440e18110b8239e3a18d
Harsha Kumar <battulaharshakumar@gamil.com>
2023-11-24 15:42:37 +0530

    movies

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css'
+
+function Movies () {
+  return(
+   <div className="main">
+    <div>
+      
+      <h2>PUSHPA</h2>
+      <p>Year:2022</p>
+      <p>Status:Hit</p>
+    </div>
+    <div>
+      <h2>AVATAR</h2>
+      <p>Year:2009</p>
+      <p>Status:Hit</p>
+    </div>
+    <div>
+      <h2>RRR</h2>
+      <p>Year:2023</p>
+      <p>Status:Hit</p>
+    </div>
+    <div>
+      <h2>BAHUBALI</h2>
+      <p>Year:2015</p>
+      <p>Status:Hit</p>
+    </div>
+    <div>
+      <h2>POKIRI</h2>
+      <p>Year:2006</p>
+      <p>Status:Hit</p>
+    </div>
+    <div>
+      <h2>KUSHI</h2>
+      <p>Year:2001</p>
+      <p>Status:Hit</p>
+    </div>
+    <div>
+      <h2>ARYA</h2>
+      <p>Year:2004</p>
+      <p>Status:Hit</p>
+    </div>
+    <div>
+      <h2>BUNNY</h2>
+      <p>Year:2005</p>
+      <p>Status:Hit</p>
+      </div>
+    <div>
+      <h2>HAPPY</h2>
+      <p>Year:2006</p>
+      <p>Status:Hit</p>    
+    </div>
+   </div>
+
+  )
+}
+export default Movies;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Movies from './App'
+
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render (
+  <React.StrictMode>
+    <body>
+    <h1>MOVIES</h1>
+    <Movies />
+    </body>
+  </React.StrictMode>
+) 
+
+
+
